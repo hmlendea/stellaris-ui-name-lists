fcdb41d9b033eea0583a59594f6ffdd2ed935990
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-04-06 23:11:32 +0300

    Extracted mexican and argentinian names

--- build.sh
+++ build.sh
@@ -93,19 +93,21 @@ build "ui_extra_humans_hellenic"        "Human - Hellenic"      "L" human/hellen
 build "ui_extra_humans_hindi"           "Human - Hindi"         "L" human/hindi
 build "ui_extra_humans_italian"         "Human - Italian"       "L" human/italian
 build "ui_extra_humans_japanese"        "Human - Japanese"      "L" human/japanese
-build "ui_extra_humans_latino"          "Human - Latino"        "L" human/brazilian human/spanish human/common_portuguese
+build "ui_extra_humans_latino"          "Human - Latino"        "L" human/argentinian human/brazilian human/mexican human/spanish human/common_portuguese
 build "ui_extra_humans_portuguese"      "Human - Portuguese"    "L" human/brazilian human/portuguese human/common_portuguese
 build "ui_extra_humans_romanian"        "Human - Romanian"      "L" human/romanian
-build "ui_extra_humans_romance"         "Human - Romance"       "L" human/brazlian human/catalan human/french human/italian human/portuguese human/roman \
-                                                                    human/romanian human/spanish human/common_portuguese
+build "ui_extra_humans_romance"         "Human - Romance"       "L" human/argentinian human/brazlian human/catalan human/french human/italian \
+                                                                    human/mexican human/portuguese human/roman human/romanian human/spanish \
+                                                                    human/common_portuguese
 build "ui_extra_humans_russian"         "Human - Russian"       "L" human/russian
 build "ui_extra_humans_slavic"          "Human - Slavic"        "L" human/bulgarian human/polish human/russian human/ukrainian human/common_slavic
 build "ui_extra_humans_spqr_extended"   "Human - Roman"         "L" human/roman human/human3
 build "ui_extra_humans_turkic"          "Human - Turkic"        "L" human/turkic
 build "ui_extra_humans_extended"        "Human - Extended"      "L" \
-      human/african human/arabic human/armenian human/austronesian human/baltic human/brazilian human/bulgarian human/catalan human/celtic human/chinese \
-      human/english human/french human/germanic human/hebrew human/hellenic human/hindi human/hungarian human/italian human/japanese human/korean \
-      human/mongol human/persian human/portuguese human/roman human/romanian human/russian human/spanish human/tibetan human/turkic human/ukrainian \
+      human/african human/arabic human/argentinian human/armenian human/austronesian human/baltic human/brazilian human/bulgarian human/catalan \
+      human/celtic human/chinese human/english human/french human/germanic human/hebrew human/hellenic human/hindi human/hungarian \
+      human/italian human/japanese human/korean human/mexican human/mongol human/persian human/portuguese human/roman human/romanian \
+      human/russian human/spanish human/tibetan human/turkic human/ukrainian \
       human/common_asian human/common_portuguese human/common_slavic human/common \
       starcraft/human starwars/human galciv/human sose/human runescape/human other-media/human \
       human/human1 human/human2 human/human3 human/zextended
