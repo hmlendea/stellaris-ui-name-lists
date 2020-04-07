392edfd3d76e83bb35e4f70b233680e848634bf8
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-04-07 18:36:51 +0300

    Extracted icelandic names into their own list

--- build.sh
+++ build.sh
@@ -90,7 +90,7 @@ build "ui_extra_humans_chinese"         "Human - Chinese"       "L" human/chines
 build "ui_extra_humans_english"         "Human - English"       "L" human/american human/australian human/canadian human/zealandian human/english
 build "ui_extra_humans_french"          "Human - French"        "L" human/french
 build "ui_extra_humans_germanic"        "Human - Germanic"      "L"
-    human/danish human/german human/norwegian human/swedish human/germanic
+    human/danish human/german human/icelandic human/norwegian human/swedish human/germanic
 build "ui_extra_humans_hellenic"        "Human - Hellenic"      "L" human/hellenic
 build "ui_extra_humans_hindi"           "Human - Hindi"         "L" human/hindi
 build "ui_extra_humans_italian"         "Human - Italian"       "L" human/italian
@@ -109,7 +109,7 @@ build "ui_extra_humans_turkic"          "Human - Turkic"        "L" human/turkic
 build "ui_extra_humans_extended"        "Human - Extended"      "L" \
       human/afghan human/african human/american human/arabic human/argentinian human/armenian human/australian human/austronesian human/baltic \
       human/brazilian human/bulgarian human/canadian human/catalan human/celtic human/chinese human/cuban human/danish human/english human/french \
-      human/german human/germanic human/guatemalan human/hebrew human/hellenic human/hindi human/hungarian human/italian \
+      human/german human/germanic human/guatemalan human/hebrew human/hellenic human/hindi human/hungarian human/icelandic human/italian \
       human/japanese human/korean human/mexican human/mongol human/norwegian human/persian human/portuguese human/roman human/romanian \
       human/russian human/spanish human/swedish human/tibetan human/turkic human/ukrainian human/venezuelan human/zealandian \
       human/common_asian human/common_portuguese human/common_slavic human/common \
