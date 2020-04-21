33245cd0d2ac6af7350d36290fb78492ff52b406
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-04-21 14:19:04 +0300

    Extracted malian names

--- build.sh
+++ build.sh
@@ -81,7 +81,8 @@ function generate-mod-descriptor {
 }
 
 build "ui_extra_humans_african"         "Human - African"           "L" \
-    human/algerian human/angolan human/congolese human/ethiopian human/mauritanian human/nigerian human/somali human/tunisian human/zambian human/african
+    human/algerian human/angolan human/congolese human/ethiopian human/malian human/mauritanian \
+    human/nigerian human/somali human/tunisian human/zambian human/african
 build "ui_extra_humans_arabic"          "Human - Arabic"            "L" human/arabic
 build "ui_extra_humans_asian"           "Human - Asian"             "L" \
     human/chinese human/japanese human/korean human/mongol human/taiwanese human/tibetan human/vietnamese human/common_asian
@@ -137,7 +138,7 @@ build "ui_extra_humans_extended"        "Human - Extended"          "L" \
     human/estonian human/ethiopian human/filipino human/french human/german human/germanic \
     human/greek human/greenlandic human/guatemalan human/hawaiian human/hebrew human/hindi \
     human/hungarian human/icelandic human/icenic human/indian human/indonesian human/italian human/irish \
-    human/japanese human/kiribatian human/korean human/latvian human/lithuanian human/malaysian \
+    human/japanese human/kiribatian human/korean human/latvian human/lithuanian human/malaysian human/malian \
     human/maori human/mauritanian human/mexican human/mongol human/nepali human/nigerian \
     human/norwegian human/pakistani human/persian human/polish human/polynesian human/portuguese \
     human/roman human/romanian human/russian human/scottish human/serbian human/slovakian \
