38fa435707c9645ecb423620095dc65b8ba9d578
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-12-07 18:52:09 +0200

    Validate and fix misplaced <Url> tags

--- scripts/validate-data.sh
+++ scripts/validate-data.sh
@@ -6,5 +6,6 @@ NAME_LISTS_DIR_PATH="$(pwd)/name-lists"
 grep -Pzo "\n\s*<(/[^>]*)>.*\n\s*<\1>\n" --recursive "${NAME_LISTS_DIR_PATH}" # Double tags
 grep -Pzo "\n\s*<([^>]*)>\s*\n\s*</\1>\n" --recursive "${NAME_LISTS_DIR_PATH}" # Empty tags
 grep -Pzo "\n\s*</Characters>\n\s*<Characters>\s*\n" --recursive "${NAME_LISTS_DIR_PATH}" # Multiple <Characters> tags
+grep -Pzo "\n\s*<Values>\n\s*<Url>" --recursive "${NAME_LISTS_DIR_PATH}" # <Url> inside <Values>
 
 grep -n "<Url>https://github.com" --recursive "${NAME_LISTS_DIR_PATH}" # Non-raw GitHub URLs
