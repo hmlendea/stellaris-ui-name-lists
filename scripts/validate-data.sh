7e7f9f9c672fe327a73ceee937609293e7ce84ad
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-12-07 18:34:38 +0200

    Improved validations

--- scripts/validate-data.sh
+++ scripts/validate-data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+NAME_LISTS_DIR_PATH="$(pwd)/name-lists"
+
 # Validate XML structure
-grep -Pzo "\n\s*<(/[^>]*)>.*\n\s*<\1>\n" name-lists/*/**.xml # Double tags
-grep -Pzo "\n\s*<([^>]*)>\s*\n\s*</\1>\n" name-lists/*/**.xml # Empty tags
+grep -Pzo "\n\s*<(/[^>]*)>.*\n\s*<\1>\n" --recursive "${NAME_LISTS_DIR_PATH}" # Double tags
+grep -Pzo "\n\s*<([^>]*)>\s*\n\s*</\1>\n" --recursive "${NAME_LISTS_DIR_PATH}" # Empty tags
+grep -Pzo "\n\s*</Characters>\n\s*<Characters>\s*\n" --recursive "${NAME_LISTS_DIR_PATH}" # Multiple <Characters> tags
