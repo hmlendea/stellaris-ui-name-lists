be4f825869d94b347e6af182ff6872a42b057e9f
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-12-08 16:56:05 +0200

    Improved validation

--- scripts/validate-data.sh
+++ scripts/validate-data.sh
@@ -4,7 +4,12 @@ function validateNameLists() {
     local PATTERN="${@}"
 
     for NAME_LIST in $(find "name-lists" -type f -name "*.xml"); do
-        grep -n "${PATTERN}" "${NAME_LIST}"
+        local FINDING=$(grep -n "${PATTERN}" "${NAME_LIST}")
+
+        if [ -n "${FINDING}" ]; then
+            echo "${NAME_LIST}:"
+            echo "${FINDING}"
+        fi
     done
 }
 
@@ -12,13 +17,19 @@ function validateNameLists_multiLine() {
     local PATTERN="${@}"
 
     for NAME_LIST in $(find "name-lists" -type f -name "*.xml"); do
-        grep -Pzo "${PATTERN}" "${NAME_LIST}"
+        local FINDING=$(grep -Pzo "${PATTERN}" "${NAME_LIST}")
+
+        if [ -n "${FINDING}" ]; then
+            echo "${NAME_LIST}:"
+            echo "${FINDING}"
+        fi
     done
 }
 
 # Validate XML structure
 validateNameLists_multiLine "\n\s*<(/[^>]*)>.*\n\s*<\1>\n" # Double tags
-validateNameLists_multiLine "\n\s*<([^>]*)>\s*\n\s*</\1>\n" # Empty tags
+validateNameLists "<\([^>]*\)>\s*</\1>" # Empty tags - single-line
+validateNameLists_multiLine "\n\s*<([^>]*)>\s*\n\s*</\1>\n" # Empty tags - multi-line
 validateNameLists_multiLine "\n\s*</Characters>\n\s*<Characters>\s*\n" # Multiple <Characters> tags
 validateNameLists_multiLine "\n\s*<Values>\n\s*<Url>" # <Url> inside <Values>
 
