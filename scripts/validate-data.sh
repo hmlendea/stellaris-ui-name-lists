d03bbe46543226ce5512e7ce4912b601521cbf46
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-12-11 13:45:35 +0200

    Improved the validation script

--- scripts/validate-data.sh
+++ scripts/validate-data.sh
@@ -24,4 +24,6 @@ validateNameLists_multiLine "\n\s*<Values>\n\s*<Url>" # <Url> inside <Values>
 
 validateNameLists_multiLine "\n\s*<[^N][a-zA-Z]*[^p]>\n\s*<Name>" # <Name> outside <NameGroup>
 
+validateNameLists_multiLine "\n(\s*)<[a-zA-Z]*>\n\1<[a-zA-Z]*>"
+
 validateNameLists "<Url>https://github.com" # Non-raw GitHub URLs
