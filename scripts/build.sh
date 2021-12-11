b2e7bcc144ba7b0463dbe47b974541c0005413b3
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-12-11 17:05:52 +0200

    Added an extra mammalian namelist

--- scripts/build.sh
+++ scripts/build.sh
@@ -334,6 +334,7 @@ build "ui_extra_hum3" "Extra - Humanoid 3"      "R" ui/hum3
 build "ui_extra_hum3" "Extra - Humanoid 4"      "R" ui/hum4
 build "ui_extra_mam1" "Extra - Mammalian 1"     "R" ui/mam1
 build "ui_extra_mam2" "Extra - Mammalian 2"     "R" ui/mam2
+build "ui_extra_mam3" "Extra - Mammalian 3"     "R" ui/mam3
 build "ui_extra_mol1" "Extra - Molluscoid 1"    "R" ui/mol1
 build "ui_extra_mol2" "Extra - Molluscoid 2"    "R" ui/mol2
 build "ui_extra_pla1" "Extra - Plantoid 1"      "R" ui/pla1
